be0cb568451bfabb2bb346f42acfbd487398b243
mihzas <sashatkachenko1993@gmail.com>
2020-05-28 11:23:17 +0300

    Lesson3 task3 try8

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -3,36 +3,36 @@
 const a = 17;
 const b = 5;
 console.log('a > b: ', a > b);
-console.log('a <= b:', a <= b);
-console.log('a == b:', a == b);
-console.log('a != b:', a != b);
+console.log('a <= b: ', a <= b);
+console.log('a == b: ', a == b);
+console.log('a != b: ', a != b);
 
 
 /* сравнение строк */
 
-console.log('\'a\' < \'b\':', 'a' < 'b');
-console.log('\'ab\' > \'b\':', 'ab' > 'b');
+console.log('\'a\' < \'b\': ', 'a' < 'b');
+console.log('\'ab\' > \'b\': ', 'ab' > 'b');
 
 /* строгое сравнение */
 
 console.log('17 === 1: ', 17 === 1);
-console.log('17 === 17:', 17 === 17);
-console.log('17 === \'17\':', 17 === '17');
-console.log('17 === true:', 17 === true);
-console.log('0 === \'\':', 0 === '');
-console.log('true === false:', true === false);
-console.log('true === true:', true === true);
-console.log('null === undefined:', null === undefined);
-console.log('false === 0:', false === 0);
+console.log('17 === 17: ', 17 === 17);
+console.log('17 === \'17\': ', 17 === '17');
+console.log('17 === true: ', 17 === true);
+console.log('0 === \'\': ', 0 === '');
+console.log('true === false: ', true === false);
+console.log('true === true: ', true === true);
+console.log('null === undefined: ', null === undefined);
+console.log('false === 0: ', false === 0);
 
 
 /* нестрогое сравнение */
 
-console.log('17 == \'17\':',17 == '17';
-console.log('\'0\' == \'\':', '0' == '');
-console.log('0 == \'\':', 0 == '');
-console.log('null == undefined:', null == undefined);
-console.log('false == 0:', false == 0);
+console.log('17 == \'17\': ',17 == '17';
+console.log('\'0\' == \'\': ', '0' == '');
+console.log('0 == \'\': ', 0 == '');
+console.log('null == undefined: ', null == undefined);
+console.log('false == 0: ', false == 0);
 
 
 /* осторожно c null и undefined */
