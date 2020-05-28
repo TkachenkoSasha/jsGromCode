34774a09ae3632dc7224682d40801a6c1da2fb51
mihzas <sashatkachenko1993@gmail.com>
2020-05-28 12:32:07 +0300

    Lesson3 task3 try1

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -11,7 +11,7 @@ console.log('a != b: ', a != b);
 /* сравнение строк */
 
 console.log('\'a\' < \'b\': ', 'a' < 'b');
-console.log('\'ab\' > \'b\': ', 'ab' > 'b');
+console.log('\'ab\' > \'a\': ', 'ab' > 'a');
 
 /* строгое сравнение */
 
@@ -19,7 +19,7 @@ console.log('17 === 1: ', 17 === 1);
 console.log('17 === 17: ', 17 === 17);
 console.log('17 === \'17\': ', 17 === '17');
 console.log('17 === true: ', 17 === true);
-console.log('0 === \'\': ', 0 === '');
+console.log("0 === '': ", 0 === '');
 console.log('true === false: ', true === false);
 console.log('true === true: ', true === true);
 console.log('null === undefined: ', null === undefined);
