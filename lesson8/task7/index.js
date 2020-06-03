d09e1c5dd45b198db91cb46b65235189924e2c18
mihzas <sashatkachenko1993@gmail.com>
2020-06-03 18:10:26 +0300

    Lesson8 task 7 try2

--- lesson8/task7/index.js
+++ lesson8/task7/index.js
@@ -4,14 +4,14 @@ function addPropertyV1(obj, key, value){
 }
 
 function addPropertyV2(obj, key, value){
-    Object.assign(obj, {key: value});
+    Object.assign(obj, {[key]: value});
     return obj;
 }
 
 function addPropertyV3(obj, key, value){
-    return Object.assign({key: value}, obj);
+    return Object.assign({[key]: value}, obj);
 }
 
 function addPropertyV4(obj, key, value){
-    return {key: value, ...obj};
+    return {[key]: value, ...obj};
 }
