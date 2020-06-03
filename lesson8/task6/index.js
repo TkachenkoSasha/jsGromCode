0698b77946f5d7b290fcd1a0bcdc00343a52af3e
mihzas <sashatkachenko1993@gmail.com>
2020-06-03 17:47:55 +0300

    Lesson7 task6 try1

---
+++ lesson8/task6/index.js
@@ -0,0 +1,17 @@
+function addPropertyV1(userData, userId){
+    userData.id = userId;
+    return userData;
+}
+
+function addPropertyV2(userData, userId){
+    Object.assign(userData, {id: userId});
+    return userData;
+}
+
+function addPropertyV3(userData, userId){
+    return Object.assign({id: userId}, userData,);
+}
+
+function addPropertyV4(userData, userId){
+    return {id: userId, ...userData};
+}
