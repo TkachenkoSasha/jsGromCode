e288803d312e7eaaed27d8866b9fcf58f7302832
mihzas <sashatkachenko1993@gmail.com>
2020-06-03 22:01:44 +0300

    Lesson9 task3 try2

--- lesson9/task3/index.js
+++ lesson9/task3/index.js
@@ -1,5 +1,5 @@
 function getCustomersList(obj){
-    return Object.entries(Object.assign({}, obj))
+    return Object.entries({...obj})
     .map(el => {el[1]['id'] = el[0]; return el[1];})
     .sort(function(el1, el2){return el1.age - el2.age});
 }
